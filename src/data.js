20fbee7a390733b09faf59900deb208ba6200d9c
CoderWorld1 <130457173+CoderWorld1@users.noreply.github.com>
2023-10-22 04:29:04 +0000

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Vaibhav Shrivastav",
+    job: "Frontend Engineer",
+    image: "https://pbs.twimg.com/media/F9BBWs4aMAAYEbh?format=jpg&name=medium",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Aniket Pal",
+    job: "Graphic Designer",
+    image: "https://pbs.twimg.com/media/F9BBTUGaEAAlxdW?format=jpg&name=medium",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Abhay Pratap Singh",
+    job: "Frontend Engineer",
+    image: "https://pbs.twimg.com/media/F9BBOV4bsAA74Qb?format=jpg&name=medium",
+    text: "I am thrilled with the results of our recent Frontend campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Saurabh Shrivastav",
+    job: "Frontend Engineer",
+    image: "https://pbs.twimg.com/media/F9BBKMsa0AAy-RP?format=jpg&name=medium",
+    text: "I have worked with many Developers over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and Debugging code that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Akarsh Chauhan",
+    job: "Data Analyst",
+    image: "https://pbs.twimg.com/media/F9BBQ2lb0AAnrH7?format=jpg&name=medium",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 6,
+    name: "Abhishek Chauhan",
+    job: "Teacher",
+    image: "https://scontent.fdel8-1.fna.fbcdn.net/v/t1.6435-9/178753064_104719231773244_2489703369911765936_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=iYgCKyJuGJEAX8ROw8r&_nc_ht=scontent.fdel8-1.fna&oh=00_AfAssAaVAIFVOkibn0YqgWMSUdQZnALdY0mdgzmEVt2Wyw&oe=6539F2B4",
+    text: "I am a teacher and I teach coaching to class 12 students. I have graduated from Chhatrapati Shahu Ji Maharaj University and have done BEd from Agra University. Recently I am doing UP D.El.Ed.",
+  },
+  {
+    id: 7,
+    name: "Ishant Shrivastav",
+    job: "Teacher",
+    image: "https://pbs.twimg.com/media/F68oQGIaYAAUrwc?format=jpg&name=4096x4096",
+    text: "I have studied Physics and Chemistry and I have very good command in both the subjects, currently I am pursuing graduation and teaching students till class 12, after graduation I am thinking of IIT.",
+  },
+];
+
+export default reviews;
