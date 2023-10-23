60e769866bf1a9524a53d1afbd2ac5f02c347fe4
CoderWorld1 <130457173+CoderWorld1@users.noreply.github.com>
2023-10-23 09:22:17 +0000

    <h1> Replace Friend to Team

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
       <div className='text-center'>
-        <h1 className='text-4xl font-bold'>Our Friends</h1>
+        <h1 className='text-4xl font-bold'>Our Team</h1>
         <div className='bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
 
         <Friends reviews={reviews} />
